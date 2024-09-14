4ca64c22c1a9bfdd6df86b168e2013fc3f6d7860
SyrillDex <syrilldexl@gmail.com>
2024-09-14 21:06:39 +0800

    Add files to git

---
+++ javascript/about.js
@@ -0,0 +1,44 @@
+const infos = [{
+  image: 'images/cvsu.jpg',
+  description: {
+    title: 'School',
+    body: 'Syrill Dex studied at Cavite State University Carmona Campus. BS in Computer Engineering.'
+  }
+},{
+  image: 'images/coding.jpg',
+  description: {
+    title: 'Web Development',
+    body: 'He is a passionate programmer specially on web develeopment.'
+  }
+}, {
+  image: 'images/mcdo.jpg',
+  description: {
+    title: 'Work experience',
+    body: 'He worked as a part-time crew at McDonalds for almost 3 years while studying.'
+  }
+}, {
+  image: 'images/drums.jpg',
+  description: {
+    title: 'Hobby',
+    body: 'He has a deep love for music, and playing the drums is his favorite hobby.'
+  }
+}];
+
+let aboutHTML = '';
+
+infos.forEach((info) => {
+  aboutHTML += `
+    <div class="about-me-card">
+        <img src="${info.image}" alt="school image">
+        <div>
+          <h2>${info.description.title}</h2>
+          <div class="card-line"></div>
+          <p>${info.description.body}</p>
+        </div>
+      </div>
+  `
+});
+
+console.log(aboutHTML);
+
+document.querySelector('.js-cards-container').innerHTML = aboutHTML;
---
+++ javascript/index.js
@@ -0,0 +1,37 @@
+
+var prevScrollpos = window.scrollY;
+let mybutton = document.getElementById("top-button");
+let section = document. querySelectorAll('section');
+
+window.onscroll = function() {
+  var currentScrollPos = window.scrollY;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("nav-div").style.top = "0px";
+    document.getElementById('img1').style.filter = "none";
+    document.getElementById('img1').style.transform = "none";
+  } else {
+    document.getElementById("nav-div").style.top = "-60px";
+    document.getElementById('img1').style.filter = "grayscale(50%)";
+    document.getElementById('img1').style.transform = "scaleX(-1)";
+  }
+  // if (currentScrollPos === 0) {
+  //   document.getElementById('nav-div').style.backgroundColor = 'transparent';
+  // }
+  prevScrollpos = currentScrollPos;
+
+  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  window.scrollTo({
+    top: 0,             // Scroll to the top of the page
+    behavior: 'smooth'  // Smooth scroll animation
+  });
+}
+
+// Attach the topFunction to the button click
+mybutton.onclick = topFunction;
